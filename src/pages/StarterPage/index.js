e5ce389c26d4a9b3da4ebcb97833a1c53c5fcbd5
shreyansh <116005536+codingmonk-yt@users.noreply.github.com>
2023-12-28 23:04:40 +0530

    first commit

---
+++ src/pages/StarterPage/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Index(props) {
+
+    document.title = " Starter Page | ChitChat"
+    return (
+        <React.Fragment>
+            
+        </React.Fragment>
+    );
+}
+
+export default Index;
